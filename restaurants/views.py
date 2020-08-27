04ac7acf343c447dbab834e0cb06de101847e6fc
Ali Al Dobyan <ali.aldobyan@gmail.com>
2020-08-27 13:36:43 +0300

    First Django project is created

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
 
+
+def homepage(request):
+
+    context = {
+        "title": "First Django project",
+        "msg" : "Hello World!"
+    }
+
+    return render(request, "index.html", context)
